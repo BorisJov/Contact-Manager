168146e7a7ae2345531253478ce83e5925bdbc41
Boris <30473641+BorisJov@users.noreply.github.com>
2017-08-15 17:39:54 +0200

    Established PhoneNumber-Person relations, errors in test?

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: "021123333", person_id: 1) }
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+  it 'must have a reference to a Person' do
+    phone_number.person_id = nil
+    expect(phone_number).not_to be_valid
+  end
+  it 'is associated with a person' do
+    expect(phone_number).to respond_to(:person)
+  end
 end
